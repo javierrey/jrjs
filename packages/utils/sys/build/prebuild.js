484e7a8855ab5ca1c0925ea33804bf89a7cb5850
unknown <javier.rey.eu@gmail.com>
2026-08-08 23:38:28 +0100

    update prebuild

--- packages/utils/sys/build/prebuild.js
+++ packages/utils/sys/build/prebuild.js
@@ -2,26 +2,42 @@
 // @ts-check
 
 import {
-  getArgumentValue, copyDir, symlinkDir,
+  getArgumentValue, fileExists, copyDir, symlinkDir,
 } from '../../../lib/sys/sys.js';
 
-const method = getArgumentValue('method') === 'symlink' ? symlinkDir : copyDir; // copy, symlink
+const cloneMethod = getArgumentValue('method') === 'symlink' ? symlinkDir : copyDir; // copy, symlink
 
-const origBase = getArgumentValue('orig') || './node_modules/jrjs/packages';
-const destBase = getArgumentValue('dest') || './packages/sample';
+const origBase = getArgumentValue('orig') || 'packages';
+const destBase = getArgumentValue('dest') || 'packages/sample';
 
-const importsCore = getArgumentValue('imports-core')?.split(',').filter(Boolean) ?? [];
-const importsView = getArgumentValue('imports-view')?.split(',').filter(Boolean) ?? [];
+// Unlike view, sys modules do need to be copied into the consumer source (sys-related params commented out).
 
-method(origBase + '/lib/core', destBase + '/view/jrjs/lib/core');
-method(origBase + '/lib/view', destBase + '/view/jrjs/lib/view');
-method(origBase + '/utils/core', destBase + '/view/jrjs/utils/core');
-method(origBase + '/utils/view', destBase + '/view/jrjs/utils/view');
+const importsToView = getArgumentValue('imports-view')?.split(',').filter(Boolean) ?? [];
+// const importsToSys = getArgumentValue('imports-sys')?.split(',').filter(Boolean) ?? [];
 
-importsCore.forEach((folder) => {
-  method(origBase + '/imports/core/' + folder, destBase + '/view/jrjs/imports/core/' + folder);
+const destView = destBase + '/view/jrjs';
+// const destSys = destBase + '/sys/jrjs';
+
+cloneMethod(origBase + '/lib/core', destView + '/lib/core');
+cloneMethod(origBase + '/lib/view', destView + '/lib/view');
+cloneMethod(origBase + '/utils/core', destView + '/utils/core');
+cloneMethod(origBase + '/utils/view', destView + '/utils/view');
+
+// cloneMethod(origBase + '/lib/core', destSys + '/lib/core');
+// cloneMethod(origBase + '/lib/sys', destSys + '/lib/sys');
+// cloneMethod(origBase + '/utils/core', destSys + '/utils/core');
+// cloneMethod(origBase + '/utils/sys', destSys + '/utils/sys');
+
+importsToView.forEach((folder) => {
+  let orig = origBase + '/imports/view/' + folder;
+  fileExists(orig) && cloneMethod(orig, destView + '/imports/view/' + folder);
+  orig = origBase + '/imports/core/' + folder;
+  fileExists(orig) && cloneMethod(orig, destView + '/imports/core/' + folder);
 });
 
-importsView.forEach((folder) => {
-  method(origBase + '/imports/view/' + folder, destBase + '/view/jrjs/imports/view/' + folder);
-});
+// importsToSys.forEach((folder) => {
+//   let orig = origBase + '/imports/sys/' + folder;
+//   fileExists(orig) && cloneMethod(orig, destSys + '/imports/sys/' + folder);
+//   orig = origBase + '/imports/core/' + folder;
+//   fileExists(orig) && cloneMethod(orig, destSys + '/imports/core/' + folder);
+// });
