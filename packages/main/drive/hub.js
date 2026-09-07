e734013467a643ee750a3067c5e56e86f27e4db7
unknown <javier.rey.eu@gmail.com>
2026-09-07 02:19:55 +0100

    remove as same name (stupid vscode refactor)

--- packages/main/drive/hub.js
+++ packages/main/drive/hub.js
@@ -21,7 +21,7 @@ import { coreHub } from '../core/hub.js';
 
 export * from '../../../../jrjs/packages/lib/drive/drive.js';
 export {
-  getEnvHubName, setupClusterWorker, stopSavedPrimaryProcess as stopSavedPrimaryProcess,
+  getEnvHubName, setupClusterWorker, stopSavedPrimaryProcess,
 } from '../../../../jrjs/packages/lib/drive/cluster.js';
 
 const _fileurl = import.meta.url;
